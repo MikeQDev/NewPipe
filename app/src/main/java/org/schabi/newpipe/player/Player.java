8de8c2962e1534ca0f38fd48d9b2f2c4aa71257d
MikeQDev <7594721+MikeQDev@users.noreply.github.com>
2023-04-06 20:56:37 -0400

    5

--- app/src/main/java/org/schabi/newpipe/player/Player.java
+++ app/src/main/java/org/schabi/newpipe/player/Player.java
@@ -1399,7 +1399,7 @@ public final class Player implements PlaybackListener, Listener {
                 break;
             case ERROR_CODE_IO_INVALID_HTTP_CONTENT_TYPE:
             case ERROR_CODE_IO_BAD_HTTP_STATUS:
-                throw new Error("Test1234");
+                throw new Error("Test12345");
             case ERROR_CODE_IO_FILE_NOT_FOUND:
             case ERROR_CODE_IO_NO_PERMISSION:
             case ERROR_CODE_IO_CLEARTEXT_NOT_PERMITTED:
